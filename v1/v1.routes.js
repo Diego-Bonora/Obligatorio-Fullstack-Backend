fbee65ee9ee148a632df748c85adc12d3dade7c7
Diego-Bonora <bonoradi@gmail.com>
2026-09-20 19:22:04 -0300

    CRUD for the recipes without Groq or any other integration, just basic crud

--- v1/v1.routes.js
+++ v1/v1.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRouter from "./routes/auth.routes.js";
+import recipeRouter from "./routes/recipe.routes.js";
 import { authenticateMiddleware } from "./middlewares/authenticate.middleware.js";
 
 const router = express.Router({ mergeParams: true });
@@ -10,7 +11,9 @@ router.use("/auth", authRouter);
 // A partir de acá, rutas protegidas
 router.use(authenticateMiddleware);
 
-// TODO: montar acá los routers de usuarios, recetas, categorias, admin, ia y nutricion
+router.use("/recipes", recipeRouter);
+
+// TODO: montar acá los routers de usuarios, categorias, admin, ia y nutricion
 // a medida que se vayan implementando.
 
 export default router;
